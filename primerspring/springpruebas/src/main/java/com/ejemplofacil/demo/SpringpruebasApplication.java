1445429f993d7524e3a2cacda9fbd109ecf19262
trontzal <trontzal@gmail.com>
2024-04-24 12:14:26 +0200

    Configuracion spring

---
+++ primerspring/springpruebas/src/main/java/com/ejemplofacil/demo/SpringpruebasApplication.java
@@ -0,0 +1,13 @@
+package com.ejemplofacil.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringpruebasApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringpruebasApplication.class, args);
+	}
+
+}
---
+++ primerspring/springpruebas/src/test/java/com/ejemplofacil/demo/SpringpruebasApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ejemplofacil.demo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringpruebasApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
